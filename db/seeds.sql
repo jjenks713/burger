cfee46013cc5ad9c54c6350dc163b38c6c0b6a8c
Chillen <josh.jenkin@live.com>
2019-02-14 20:29:14 -0700

    server

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Flame Broiled', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fuku', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Baco Chesbugger', true);
+
